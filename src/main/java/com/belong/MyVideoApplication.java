55b972d5ebeb54f68d5d011f3b025261a80806a3
belong <belong.belong@outlook.com>
2017-01-15 13:13:00 +0800

    My_Video-1.0.0 搭建完SpringBoot-----pom

---
+++ src/main/java/com/belong/MyVideoApplication.java
@@ -0,0 +1,12 @@
+package com.belong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVideoApplication.class, args);
+	}
+}
